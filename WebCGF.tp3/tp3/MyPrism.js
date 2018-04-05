48b90b14867c294d584821de9d19f1b5810a80db
Camolas <up201202445@fe.up.pt>
2018-04-05 18:20:37 +0100

    Prisma

--- WebCGF.tp3/tp3/MyPrism.js
+++ WebCGF.tp3/tp3/MyPrism.js
@@ -2,55 +2,76 @@
 * MyQuad
 * @constructor
 */
-class MyPrism extends CGFobject
-{
+	class MyPrism extends CGFobject
+	{
 
 	constructor(scene, slices, stacks)
 	{
 		super(scene);
 
-		this.slices = slices;
-		this.stacks = stacks;
+	this.slices= slices;
+	this.stacks= stacks;
 
-		this.initBuffers();
-	};
+	this.initBuffers();
+
+	this.vertices = [];
+
+	this.indices = [];
+
+	this.normals = [];
+
+	//propriedade de hoisting!?, go tester this
+
+	var angle = 0;
+	let degtoRad = Math.PI /* * angle */ / 180;
+
+	//let rad = 2*Math.PI;
+	let incrAngle = 2 * Math.PI / this.slices;
 	
-	initBuffers()
-	{
-		this.vertices = [];
+	let z = 0;
+	let z_incr = 1 / this.stacks;
 
-		this.indices = [];
+	let inaux = 0;
 
-		this.normals = [];
-
-		//propriedade de hoisting!?, go tester this
-
-		let angle = 0;
-		let degtoRad = Math.PI /* * angle */ /180 ;
-
-		//let rad = 2*Math.PI;
-		let incrAngle = 2 * Math.PI/ this.slices;
-
-		let z = 0;
-		let z_inc = 1/this.stacks;
-
-		let inaux = 0;
-
+	for (var f = 0; f < this.stacks; f++) {
 		//this.vertices = [];
-		for (let i=0; i < this.slices; i++)
-		{
-			this.vertices.push(Math.cos(angle), Math.sin(angle),z);
-			this.vertices.push(Math.cos(angle), Math.sin(angle),z + incrAngle);
+		
+		for (let i = 0; i < this.slices; i++) {
+			this.vertices.push(Math.cos(angle), Math.sin(angle), z);
+			this.vertices.push(Math.cos(angle), Math.sin(angle), z + z_incr);
 			angle += incrAngle;
-			this.vertices.push(Math.cos(angle), Math.sin(angle),z);
-			this.vertices.push(Math.cos(angle), Math.sin(angle),z);
+			this.vertices.push(Math.cos(angle), Math.sin(angle), z);
+			this.vertices.push(Math.cos(angle), Math.sin(angle), z + z_incr);
 		}
 
+		// this.índices = [];
 
+		for (var i = 0; i < this.slices; i++) {
+			this.indices.push(i * 4 + 0 + inaux, i * 4 + 2 + inaux, i * 4 + 1 + inaux);
+		}
+		for (var i = 0; i < this.slices; i++) {
+			this.indices.push(i * 4 + 3 + inaux, i * 4 + 2 + inaux, i * 4 + 1 + inaux);
+		}
 
+		angle = 0;
+		let angle_next = 0;
 
+		//this.normals = [];
+		for (var i = 0; i < this.slices; i++) {
+			this.normals.push(Math.cos(angle / 2), Math.sin(angle / 2), 0);
+			this.normals.push(Math.cos(angle / 2), Math.sin(angle / 2), 0);
+			this.normals.push(Math.cos(angle / 2), Math.sin(angle / 2), 0);
+			this.normals.push(Math.cos(angle / 2), Math.sin(angle / 2), 0);
 
-		this.primitiveType = this.scene.gl.TRIANGLES;
-		this.initGLBuffers();
-	};
+		}
+		inaux += this.slices;
+		z += z_incr;
+
+		inaux += this.slices;
+		z += z_incr;
+
+	}
+	this.primitiveType = this.scene.gl.TRIANGLES;
+	this.initGLBuffers();
 };
+	};
